008396a4180a5c6188827a510af5f0d3fc4d2b28
martos93 <jesusma93@gmail.com>
2017-05-23 17:52:53 +0200

    CU33 y CU34 casi acabados

---
+++ src/main/java/controllers/alumn/PartnerInvitationAlumnController.java
@@ -0,0 +1,100 @@
+
+package controllers.alumn;
+
+import java.util.Collection;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import controllers.AbstractController;
+import domain.Alumn;
+import domain.PartnerInvitation;
+import services.AlumnService;
+import services.PartnerInvitationService;
+
+@Controller
+@RequestMapping("/partnerInvitation/alumn")
+public class PartnerInvitationAlumnController extends AbstractController {
+
+	@Autowired
+	private PartnerInvitationService	partnerInvitationService;
+
+	@Autowired
+	private AlumnService				alumnService;
+
+
+	@RequestMapping(value = "/listReceived", method = RequestMethod.GET)
+	public ModelAndView list() {
+		final ModelAndView res = new ModelAndView("partnerInvitation/list");
+		final Alumn alumn = this.alumnService.findByPrincipal();
+
+		final Collection<PartnerInvitation> inv = this.partnerInvitationService.findReceivedByAlumn(alumn.getId());
+		res.addObject("partnerInvitations", inv);
+		res.addObject("requestURI", "partnerInvitation/alumn/listReceived.do");
+		res.addObject("received", true);
+		return res;
+
+	}
+
+	@RequestMapping(value = "/listSended", method = RequestMethod.GET)
+	public ModelAndView listSended() {
+		final ModelAndView res = new ModelAndView("partnerInvitation/list");
+		final Alumn alumn = this.alumnService.findByPrincipal();
+
+		final Collection<PartnerInvitation> inv = this.partnerInvitationService.findSendedByAlumn(alumn.getId());
+		res.addObject("partnerInvitations", inv);
+		res.addObject("requestURI", "partnerInvitation/alumn/listSended.do");
+		res.addObject("received", false);
+		return res;
+
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int alumnId) {
+		final ModelAndView res = new ModelAndView("partnerInvitation/create");
+		final PartnerInvitation inv = this.partnerInvitationService.create(alumnId);
+		Assert.notNull(inv);
+		res.addObject("partnerInvitation", inv);
+		res.addObject("requestURI", "partnerInvitation/alumn/create.do");
+		return res;
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST, params = "save")
+	public ModelAndView create(@Valid final PartnerInvitation partnerInvitation, final BindingResult binding) {
+
+		ModelAndView res = new ModelAndView();
+
+		if (binding.hasErrors()) {
+			res = new ModelAndView("partnerInvitation/create");
+			res.addObject("requestURI", "partnerInvitation/alumn/create.do");
+			res.addObject("partnerInvitation", partnerInvitation);
+
+		} else
+			try {
+				this.partnerInvitationService.save(partnerInvitation);
+				res = new ModelAndView("partnerInvitation/list");
+				final Alumn alumn = this.alumnService.findByPrincipal();
+
+				final Collection<PartnerInvitation> inv = this.partnerInvitationService.findSendedByAlumn(alumn.getId());
+				res.addObject("partnerInvitations", inv);
+				res.addObject("requestURI", "partnerInvitation/alumn/listSended.do");
+				res.addObject("received", false);
+			} catch (final ConstraintViolationException oops) {
+				res = new ModelAndView("partnerInvitation/create");
+				res.addObject("partnerInvitation", partnerInvitation);
+				res.addObject("message", "partnerInvitation.commit.error");
+			}
+		return res;
+
+	}
+
+}
---
+++ src/main/java/services/PartnerInvitationService.java
@@ -0,0 +1,64 @@
+
+package services;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import domain.PartnerInvitation;
+import repositories.PartnerInvitationRepository;
+
+@Service
+@Transactional
+public class PartnerInvitationService {
+
+	@Autowired
+	private PartnerInvitationRepository	partnerInvitationRepository;
+
+	@Autowired
+	private AlumnService				alumnService;
+
+
+	// Constructor
+	public PartnerInvitationService() {
+		super();
+	}
+
+	// Simple CRUD methods
+
+	public PartnerInvitation create(final int alumnId) {
+		final PartnerInvitation res = new PartnerInvitation();
+		res.setState("PENDING");
+		res.setInvitationSender(this.alumnService.findByPrincipal());
+		res.setInvitationReceiver(this.alumnService.findOne(alumnId));
+
+		return res;
+	}
+
+	public Collection<PartnerInvitation> findAll() {
+		return this.partnerInvitationRepository.findAll();
+	}
+
+	public PartnerInvitation findOne(final int id) {
+		PartnerInvitation result;
+		result = this.partnerInvitationRepository.findOne(id);
+		return result;
+	}
+
+	public PartnerInvitation save(final PartnerInvitation partnerInvitation) {
+
+		return this.partnerInvitationRepository.save(partnerInvitation);
+	}
+
+	public Collection<PartnerInvitation> findReceivedByAlumn(final int id) {
+		return this.partnerInvitationRepository.findReceivedByAlumn(id);
+	}
+
+	public Collection<PartnerInvitation> findSendedByAlumn(final int id) {
+		return this.partnerInvitationRepository.findSendedByAlumn(id);
+	}
+
+}
