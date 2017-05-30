a37af18b9a1751beb7356b3a13521921c33efc69
martos93 <jesusma93@gmail.com>
2017-05-30 16:25:57 +0200

    Arreglado el mostrar clases, si no tienes un pago activo en esa clase no puedes ver materiales ni examenes.

--- src/main/java/controllers/alumn/DanceTestAlumnController.java
+++ src/main/java/controllers/alumn/DanceTestAlumnController.java
@@ -1,8 +1,9 @@
 
 package controllers.alumn;
 
-import domain.Alumn;
-import domain.DanceTest;
+import java.util.Collection;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
@@ -12,58 +13,71 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import controllers.AbstractController;
+import domain.Alumn;
 import domain.DanceClass;
+import domain.DanceTest;
+import domain.Payment;
 import services.AlumnService;
 import services.DanceClassService;
 import services.DanceTestService;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
 
 @Controller
 @RequestMapping("/danceTest/alumn")
 public class DanceTestAlumnController extends AbstractController {
 
 	@Autowired
-	DanceTestService danceTestService;
+	DanceTestService			danceTestService;
 
 	@Autowired
-	private DanceClassService danceClassService;
+	private DanceClassService	danceClassService;
 
 	@Autowired
-	private AlumnService alumnService;
+	private AlumnService		alumnService;
 
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public ModelAndView list(@RequestParam final int classId) {
 		final ModelAndView res = new ModelAndView("danceTest/list");
 
-		res.addObject("danceTests", danceTestService.findDanceTestsAvailableByDanceClass(classId));
-		res.addObject("myDanceTests", danceTestService.findDanceTestsByAlumn(alumnService.findByPrincipal().getId()));
-		res.addObject("requestURI", "danceTest/list.do");
+		try {
+			final DanceClass dc = this.danceClassService.findOne(classId);
+			final Alumn alumn = this.alumnService.findByPrincipal();
+			boolean paid = true;
+			final Date today = new Date(System.currentTimeMillis() - 10000);
+			final Collection<Payment> payments = alumn.getPayments();
+			for (final Payment p : payments)
+				if (p.getEndDate().getTime() < today.getTime() && p.getDanceClass().getId() == classId)
+					paid = false;
+			Assert.isTrue(paid);
+			res.addObject("danceTests", this.danceTestService.findDanceTestsAvailableByDanceClass(classId));
+			res.addObject("myDanceTests", this.danceTestService.findDanceTestsByAlumn(this.alumnService.findByPrincipal().getId()));
+			res.addObject("requestURI", "danceTest/list.do");
+		} catch (final Throwable oops) {
+			res.addObject("message", "danceTest.payment.error");
+		}
 		return res;
 
 	}
 
 	@RequestMapping(value = "/joinin", method = RequestMethod.GET)
-	public ModelAndView joinin(@RequestParam int danceTestId){
-		ModelAndView res = new ModelAndView("danceTest/list");;
-		DanceTest danceTest = danceTestService.findOne(danceTestId);
+	public ModelAndView joinin(@RequestParam final int danceTestId) {
+		final ModelAndView res = new ModelAndView("danceTest/list");
+		;
+		final DanceTest danceTest = this.danceTestService.findOne(danceTestId);
 
-		try{
-			Alumn alumn = alumnService.findByPrincipal();
+		try {
+			final Alumn alumn = this.alumnService.findByPrincipal();
 			Assert.isTrue(danceTest.getLimitInscription().after(new Date()));
-			Assert.isTrue(!danceTestService.findDanceTestsByAlumn(alumn.getId()).contains(danceTest) && danceTestService.danceTestsCanJoinIn(alumn.getId()).contains(danceTest));
+			Assert.isTrue(!this.danceTestService.findDanceTestsByAlumn(alumn.getId()).contains(danceTest) && this.danceTestService.danceTestsCanJoinIn(alumn.getId()).contains(danceTest));
 
-			danceTestService.joinInDanceTest(danceTestId);
+			this.danceTestService.joinInDanceTest(danceTestId);
 
-			res.addObject("danceTests", danceTestService.findDanceTestsAvailableByDanceClass(danceTest.getDanceClass().getId()));
-			res.addObject("myDanceTests", danceTestService.findDanceTestsByAlumn(alumn.getId()));
+			res.addObject("danceTests", this.danceTestService.findDanceTestsAvailableByDanceClass(danceTest.getDanceClass().getId()));
+			res.addObject("myDanceTests", this.danceTestService.findDanceTestsByAlumn(alumn.getId()));
 			res.addObject("requestURI", "danceTest/list.do");
-		}catch (Throwable oops){
-			res.addObject("danceTests", danceTestService.findDanceTestsAvailableByDanceClass(danceTest.getDanceClass().getId()));
-			res.addObject("myDanceTests", danceTestService.findDanceTestsByAlumn(alumnService.findByPrincipal().getId()));
+		} catch (final Throwable oops) {
+			res.addObject("danceTests", this.danceTestService.findDanceTestsAvailableByDanceClass(danceTest.getDanceClass().getId()));
+			res.addObject("myDanceTests", this.danceTestService.findDanceTestsByAlumn(this.alumnService.findByPrincipal().getId()));
 			res.addObject("requestURI", "danceTest/list.do");
 			res.addObject("message", "danceTest.join.error");
 		}
--- src/main/java/controllers/alumn/MaterialAlumnController.java
+++ src/main/java/controllers/alumn/MaterialAlumnController.java
@@ -1,15 +1,22 @@
 
 package controllers.alumn;
 
+import java.util.Collection;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import controllers.AbstractController;
+import domain.Alumn;
 import domain.DanceClass;
+import domain.Payment;
+import services.AlumnService;
 import services.DanceClassService;
 import services.MaterialService;
 
@@ -21,16 +28,31 @@ public class MaterialAlumnController extends AbstractController {
 	private MaterialService		materialService;
 	@Autowired
 	private DanceClassService	danceClassService;
+	@Autowired
+	private AlumnService		alumnService;
 
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public ModelAndView list(@RequestParam final int classId) {
+
 		final ModelAndView res = new ModelAndView("material/list");
-		final DanceClass dc = this.danceClassService.findOne(classId);
+		try {
+			final DanceClass dc = this.danceClassService.findOne(classId);
+			final Alumn alumn = this.alumnService.findByPrincipal();
+			boolean paid = true;
+			final Date today = new Date(System.currentTimeMillis() - 10000);
+			final Collection<Payment> payments = alumn.getPayments();
+			for (final Payment p : payments)
+				if (p.getEndDate().getTime() < today.getTime() && p.getDanceClass().getId() == classId)
+					paid = false;
+			Assert.isTrue(paid);
+			res.addObject("materials", dc.getMaterials());
+			res.addObject("requestURI", "material/alumn/list.do");
+			return res;
+		} catch (final Throwable oops) {
+			res.addObject("message", "material.payment.error");
+		}
 
-		res.addObject("materials", dc.getMaterials());
-		res.addObject("requestURI", "material/alumn/list.do");
 		return res;
-
 	}
 }
